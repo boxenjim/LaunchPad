a90d2effaeec061989de0a5c1c574ff648a8fd15
Jim Schultz <boxenjim@gmail.com>
2015-03-16 10:31:06 -0700

    Initial Commit

---
+++ LaunchPad/LaunchPad.h
@@ -0,0 +1,19 @@
+//
+//  LaunchPad.h
+//  LaunchPad
+//
+//  Created by Jim Schultz on 3/16/15.
+//  Copyright (c) 2015 Blue Boxen, LLC/. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaunchPad.
+FOUNDATION_EXPORT double LaunchPadVersionNumber;
+
+//! Project version string for LaunchPad.
+FOUNDATION_EXPORT const unsigned char LaunchPadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchPad/PublicHeader.h>
+
+
